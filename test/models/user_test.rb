bd30ce89a8454d2e021c6e8855a6cae1ef679c21
John Winslow <winslow.johnm@gmail.com>
2017-05-25 15:07:53 -0700

    Add validations & tests for email uniqueness & downcasing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -30,6 +30,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
   test "email validation should accept valid addresses" do
     valid_addresses = %w[user1@test.com USER2@example.COM USER-3@te.st.org user.4@te.st user+5@foo.bar]
     valid_addresses.each do |valid_address|
@@ -44,6 +51,13 @@ class UserTest < ActiveSupport::TestCase
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
+  end
+
+  test "email addresses should be saved as lower case" do
+    mixed_case_email = "UsEr@TeST.coM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
 end
