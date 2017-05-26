ae8f38d14bb2292d054b68f1bb5392d5064bc633
John Winslow <winslow.johnm@gmail.com>
2017-05-25 19:16:38 -0700

    Add flash messages for signup, resent database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # handle successful save
+      flash[:success] = "Welcome to the Twitter Clone!"
+      redirect_to @user
     else 
       render 'new'
     end
