12e5ba09a5f3a92c3c9e6bdf1e6d3bf0900c416b
John Winslow <winslow.johnm@gmail.com>
2017-05-25 22:18:10 -0700

    Add session log in helper to user in controller, update header Log in to correct path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log user in and redirect to show page
+      log_in user
+      redirect_to user
     else
-      # Create an error message
       flash.now[:danger] = 'Invalid email/password combination' # Come back to this
       render 'new'
     end
