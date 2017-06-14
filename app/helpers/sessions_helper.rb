c76569c901a192a959b20e405801dfebb7e17008
John Winslow <winslow.johnm@gmail.com>
2017-06-14 02:04:13 -0700

    tests for friendly forwarding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,11 @@ module SessionsHelper
     @current_user = nil
   end
 
+# checks if a given user is the current user
+  def current_user?(user)
+    user == current_user
+  end
+
 # returns the current (logged in) user, if any
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
@@ -21,5 +26,15 @@ module SessionsHelper
     !current_user.nil?
   end
 
+# redirects to stored location (or to the default)
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+# stores the URL trying to be accessed
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 
 end
